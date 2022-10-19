b242303a69d5ba2f92a45fb8160902207b4e8a10
Ahad Ahmadi <ab.ahmadiofficial@gmail.com>
2022-10-19 23:35:17 +0430

    create all routes for dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,49 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { colorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
+import Dashboard from "./scenes/dashboard";
+import Sidebar  from "./scenes/global/Sidebar";
+import { Router, Routes, Route } from 'react-router-dom'
+import Team from "./scenes/team";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
+import Bar from "./scenes/bar";
+import Form from "./scenes/form";
+import Pie from "./scenes/pie";
+import FAQ from "./scenes/faq";
+import Geography from "./scenes/geography";
+import Calender from "./scenes/calender";
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [theme, colorMode] = useMode();
 
   return (
-    <div>
-        <h1 className='text-5xl'>Hello </h1>
-    </div>
-  )
+    <colorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div>
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path='/' element={<Dashboard />} />
+              <Route path='/team' element={<Team />} />
+              <Route path='/invoices' element={<Invoices />} />
+              <Route path='/contacts' element={<Contacts />} />
+              <Route path='/bar' element={<Bar />} />
+              <Route path='/form' element={<Form />} />
+              <Route path='/pie' element={<Pie />} />
+              <Route path='/faq' element={<FAQ />} />
+              <Route path='/geography' element={<Geography />} />
+              <Route path='/calender' element={<Calender />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </colorModeContext.Provider>
+  );
 }
 
-export default App
+export default App;
