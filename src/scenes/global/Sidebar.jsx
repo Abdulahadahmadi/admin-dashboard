f7ea1766920b9945cce4ef6c6af0c9b6165de720
Ahad Ahmadi <ab.ahmadiofficial@gmail.com>
2022-10-20 07:33:46 +0430

    Add all pages in sidebar

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,4 +1,22 @@
 import React from 'react'
+import { useState } from "react";
+import { ProSidebar, Menu, MenuItem } from 'react-pro-sidebar';
+import { Box, IconButton, Typography } from "@mui/material";
+import { Link } from 'react-router-dom'
+import { Tokens } from "../../theme";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import CalenderOutlinedIcon from "@mui/icons-material/CalenderOutlined";
+import HelpOutlinedIcon from "@mui/icons-material/HelpOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlinedIcon from "@mui/icons-material/PieChartOutlined";
+import TimeLineOutlinedIcon from "@mui/icons-material/TimeLineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+
 
 const Sidebar = () => {
   return (
