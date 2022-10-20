637aac0a68b3b6373535d3ca251bb8acb3b44778
Ahad Ahmadi <ab.ahmadiofficial@gmail.com>
2022-10-20 07:34:07 +0430

    Add import of router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import { useState } from "react";
 import { colorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from 'react-router-dom'
 import Topbar from "./scenes/global/Topbar";
-import Dashboard from "./scenes/dashboard";
 import Sidebar  from "./scenes/global/Sidebar";
-import { Router, Routes, Route } from 'react-router-dom'
-import Team from "./scenes/team";
-import Invoices from "./scenes/invoices";
-import Contacts from "./scenes/contacts";
-import Bar from "./scenes/bar";
-import Form from "./scenes/form";
-import Pie from "./scenes/pie";
-import FAQ from "./scenes/faq";
-import Geography from "./scenes/geography";
-import Calender from "./scenes/calender";
+import Dashboard from "./scenes/dashboard";
+// import Team from "./scenes/team";
+// import Invoices from "./scenes/invoices";
+// import Contacts from "./scenes/contacts";
+// import Bar from "./scenes/bar";
+// import Form from "./scenes/form";
+// import Pie from "./scenes/pie";
+// import FAQ from "./scenes/faq";
+// import Geography from "./scenes/geography";
+// import Calender from "./scenes/calender";
 
 
 
@@ -25,19 +25,20 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div>
+          <Sidebar />
           <main className="content">
             <Topbar />
             <Routes>
               <Route path='/' element={<Dashboard />} />
-              <Route path='/team' element={<Team />} />
-              <Route path='/invoices' element={<Invoices />} />
-              <Route path='/contacts' element={<Contacts />} />
-              <Route path='/bar' element={<Bar />} />
-              <Route path='/form' element={<Form />} />
-              <Route path='/pie' element={<Pie />} />
-              <Route path='/faq' element={<FAQ />} />
-              <Route path='/geography' element={<Geography />} />
-              <Route path='/calender' element={<Calender />} />
+              {/* <Route path='/team' element={<Team />} /> */}
+              {/* <Route path='/invoices' element={<Invoices />} /> */}
+              {/* <Route path='/contacts' element={<Contacts />} /> */}
+              {/* <Route path='/bar' element={<Bar />} /> */}
+              {/* <Route path='/form' element={<Form />} /> */}
+              {/* <Route path='/pie' element={<Pie />} /> */}
+              {/* <Route path='/faq' element={<FAQ />} /> */}
+              {/* <Route path='/geography' element={<Geography />} /> */}
+              {/* <Route path='/calender' element={<Calender />} /> */}
             </Routes>
           </main>
         </div>
